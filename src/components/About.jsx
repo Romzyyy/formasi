b22b8a9095ee438f4ed3c38617e241c8ead6664b
romzyyy <fijay0117@gmail.com>
2023-09-01 23:03:38 +0700

    change to font-base

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className='-mt-64 text-zinc-950 w-screen px-6'>
             <p className='text-xl tracking-wide'>
-                <span className='font-semibold text-2xl mr-2'>
+                <span className='font-semibold text-xl mr-2'>
                     Forum Mahasiswa Sistem Informasi
                 </span>
                 adalah kesatuan rencana belajar yang mengkaji, menerapkan, dan
@@ -15,7 +15,7 @@ export default function About() {
             <div className='flex flex-col my-8 gap-8'>
                 <div>
                     <h1 className='font-semibold text-xl mb-1'>VISI</h1>
-                    <p className='text-lg'>
+                    <p className='text-base'>
                         Menjadi pusat pendidikan tinggi dan pengembangan
                         keilmuan Teknik Industri yang unggul melalui
                         penyelenggaraan Tri Dharma Perguruan Tinggi yang
@@ -25,23 +25,23 @@ export default function About() {
                 </div>
                 <div className='text-right'>
                     <h1 className='font-semibold text-xl mb-1'>MISI</h1>
-                    <p className='text-lg'>
+                    <p className='text-base'>
                         Menyelenggarakan program pendidikan tinggi Teknik
                         Industri yang relevan dengan kebutuhan industri dan
                         masyarakat untuk menghasilkan sarjana yang bermoral,
                         berkemampuan akademik, dan berjiwa entrepreneur.
                     </p>
-                    <p className='mt-2 text-lg'>
+                    <p className='mt-2 text-base'>
                         Melakukan penelitian dan pengabdian kepada masyarakat
                         dalam rangka mengembangkan keilmuan Teknik Industri dan
                         membantu menyelesaikan permasalahan industri.
                     </p>
-                    <p className='mt-2 text-lg'>
+                    <p className='mt-2 text-base'>
                         Memberikan pelayanan kepada stakeholder melalui
                         pendidikan, penelitian, dan pengabdian kepada
                         masyarakat.
                     </p>
-                    <p className='mt-2 text-lg'>
+                    <p className='mt-2 text-base'>
                         Membangun jaringan kerjasama guna perbaikan pendidikan,
                         peningkatan mutu operasional Tri Dharma Perguruan
                         Tinggi, dan kerjasama penelitian.
