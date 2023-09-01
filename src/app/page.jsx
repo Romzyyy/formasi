52829c329c35514b0f4fec3f3c9affee086a16b9
romzyyy <fijay0117@gmail.com>
2023-09-01 22:53:15 +0700

    modify font card

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,8 +9,8 @@ export default function Home() {
         <main className='flex min-h-screen flex-col items-center justify-center'>
             <Slider />
             <About />
-            <div className='flex flex-col w-full mb-20 px-8'>
-                <h1 className='mb-4 text-2xl font-semibold'>
+            <div className='flex flex-col w-full mb-20 px-6'>
+                <h1 className='mb-4 text-xl font-semibold'>
                     New Information
                 </h1>
                 <div className='grid grid-rows-4 gap-4'>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 export default function Card({ title, desc }) {
     return (
         <div className='card w-full bg-gray-100 text-stone-950 hover:shadow-xl hover:bg-gray-50'>
-            <div className='card-body rounded-lg'>
-                <h2 className='card-title'>{title}</h2>
-                <div className='w-full h-32 bg-blue- overflow-hidden'>
+            <div className='card-body rounded-none'>
+                <h2 className='card-title text-base'>{title}</h2>
+                <div className='w-full h-28 bg-blue- overflow-hidden'>
                     <p>{desc}</p>
                 </div>
                 <div className='card-actions w-fit'>
