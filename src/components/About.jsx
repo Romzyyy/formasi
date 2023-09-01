fca72a24639afdb0057574910fbd63ece37a3f8f
romzyyy <fijay0117@gmail.com>
2023-09-01 22:26:00 +0700

    modify visi-misi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 export default function About() {
     return (
-        <div className='-mt-64 text-zinc-950 w-screen px-8'>
-            <p className='text-2xl tracking-widest'>
-                <span className='font-semibold text-3xl mr-2'>
+        <div className='-mt-72 text-zinc-950 w-screen px-8'>
+            <p className='text-xl tracking-widest'>
+                <span className='font-semibold text-2xl mr-2'>
                     Forum Mahasiswa Sistem Informasi
                 </span>
                 adalah kesatuan rencana belajar yang mengkaji, menerapkan, dan
@@ -10,19 +10,12 @@ export default function About() {
                 Arah kajian keilmuan dari program studi ini mencakup disiplin,
                 proses, teknik dan alat bantu yang dibutuhkan dalam rekayasa
                 sistem informasi yang meliputi tahap perencanaan, pembangunan,
-                implementasi dan pemeliharaan. Lulusan Sistem Informasi meliputi
-                pengembangan sistem informasi, metode dan peralatannya (bahasa
-                pemrograman dan database), Manajemen sistem informasi dan
-                rekayasa sistem informasi. Program studi sistem informasi saat
-                ini sangat diminati, karena di era globalisasi saat ini banyak
-                yang memerlukan ahli atau lulusan yang mahir dalam bidang
-                digital. Maka dari itu lulusan dari program studi ini sangat
-                banyak.
+                implementasi dan pemeliharaan.
             </p>
-            <div className='flex flex-row justify-between my-20 gap-20'>
+            <div className='flex flex-col my-8 gap-8'>
                 <div>
-                    <h1 className='font-semibold text-3xl mb-4'>VISI</h1>
-                    <p className='text-xl font-normal'>
+                    <h1 className='font-medium text-2xl mb-4'>VISI</h1>
+                    <p className='text-lg'>
                         Menjadi pusat pendidikan tinggi dan pengembangan
                         keilmuan Teknik Industri yang unggul melalui
                         penyelenggaraan Tri Dharma Perguruan Tinggi yang
@@ -31,24 +24,24 @@ export default function About() {
                     </p>
                 </div>
                 <div className='text-right'>
-                    <h1 className='font-semibold text-3xl mb-4'>MISI</h1>
-                    <p className='text-xl font-normal'>
+                    <h1 className='font-medium text-2xl mb-4'>MISI</h1>
+                    <p className='text-lg'>
                         Menyelenggarakan program pendidikan tinggi Teknik
                         Industri yang relevan dengan kebutuhan industri dan
                         masyarakat untuk menghasilkan sarjana yang bermoral,
                         berkemampuan akademik, dan berjiwa entrepreneur.
                     </p>
-                    <p className='text-xl mt-4 font-normal'>
+                    <p className='mt-4 text-lg'>
                         Melakukan penelitian dan pengabdian kepada masyarakat
                         dalam rangka mengembangkan keilmuan Teknik Industri dan
                         membantu menyelesaikan permasalahan industri.
                     </p>
-                    <p className='text-xl mt-4 font-normal'>
+                    <p className='mt-4 text-lg'>
                         Memberikan pelayanan kepada stakeholder melalui
                         pendidikan, penelitian, dan pengabdian kepada
                         masyarakat.
                     </p>
-                    <p className='text-xl mt-4 font-normal'>
+                    <p className='mt-4 text-lg'>
                         Membangun jaringan kerjasama guna perbaikan pendidikan,
                         peningkatan mutu operasional Tri Dharma Perguruan
                         Tinggi, dan kerjasama penelitian.
