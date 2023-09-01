ab7e3242b6016759a5a1ca0a129fe654bab46bfd
romzyyy <fijay0117@gmail.com>
2023-09-01 22:33:55 +0700

    modify font size visi-misi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
-        <div className='-mt-72 text-zinc-950 w-screen px-8'>
-            <p className='text-xl tracking-widest'>
+        <div className='-mt-64 text-zinc-950 w-screen px-8'>
+            <p className='text-xl tracking-wide'>
                 <span className='font-semibold text-2xl mr-2'>
                     Forum Mahasiswa Sistem Informasi
                 </span>
@@ -14,17 +14,17 @@ export default function About() {
             </p>
             <div className='flex flex-col my-8 gap-8'>
                 <div>
-                    <h1 className='font-medium text-2xl mb-4'>VISI</h1>
+                    <h1 className='font-semibold text-xl mb-2'>VISI</h1>
                     <p className='text-lg'>
                         Menjadi pusat pendidikan tinggi dan pengembangan
                         keilmuan Teknik Industri yang unggul melalui
                         penyelenggaraan Tri Dharma Perguruan Tinggi yang
                         berkontribusi dalam pembangunan bangsa dan masyarakat
-                        global pada tahun 2025
+                        global pada tahun 2025.
                     </p>
                 </div>
                 <div className='text-right'>
-                    <h1 className='font-medium text-2xl mb-4'>MISI</h1>
+                    <h1 className='font-semibold text-xl mb-2'>MISI</h1>
                     <p className='text-lg'>
                         Menyelenggarakan program pendidikan tinggi Teknik
                         Industri yang relevan dengan kebutuhan industri dan
