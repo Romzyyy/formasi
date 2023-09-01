9f2cc6d3e48df8fd7331b69dad8f12a894d10bd4
romzyyy <fijay0117@gmail.com>
2023-09-01 23:07:29 +0700

    removing 1 misi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,11 @@ export default function About() {
                 adalah kesatuan rencana belajar yang mengkaji, menerapkan, dan
                 mengembangkan ilmu yang melandasi rekayasa sistem informasi.
                 Arah kajian keilmuan dari program studi ini mencakup disiplin,
-                proses, teknik dan alat bantu yang dibutuhkan dalam rekayasa
-                sistem informasi yang meliputi tahap perencanaan, pembangunan,
-                implementasi dan pemeliharaan.
+                proses.
             </p>
             <div className='flex flex-col my-8 gap-8'>
                 <div>
-                    <h1 className='font-semibold text-xl mb-1'>VISI</h1>
+                    <h1 className='font-semibold text-lg mb-1'>VISI</h1>
                     <p className='text-base'>
                         Menjadi pusat pendidikan tinggi dan pengembangan
                         keilmuan Teknik Industri yang unggul melalui
@@ -24,12 +22,11 @@ export default function About() {
                     </p>
                 </div>
                 <div className='text-right'>
-                    <h1 className='font-semibold text-xl mb-1'>MISI</h1>
+                    <h1 className='font-semibold text-lg mb-1'>MISI</h1>
                     <p className='text-base'>
                         Menyelenggarakan program pendidikan tinggi Teknik
                         Industri yang relevan dengan kebutuhan industri dan
-                        masyarakat untuk menghasilkan sarjana yang bermoral,
-                        berkemampuan akademik, dan berjiwa entrepreneur.
+                        masyarakat untuk menghasilkan sarjana yang bermoral.
                     </p>
                     <p className='mt-2 text-base'>
                         Melakukan penelitian dan pengabdian kepada masyarakat
@@ -40,11 +37,6 @@ export default function About() {
                         Memberikan pelayanan kepada stakeholder melalui
                         pendidikan, penelitian, dan pengabdian kepada
                         masyarakat.
-                    </p>
-                    <p className='mt-2 text-base'>
-                        Membangun jaringan kerjasama guna perbaikan pendidikan,
-                        peningkatan mutu operasional Tri Dharma Perguruan
-                        Tinggi, dan kerjasama penelitian.
                     </p>
                 </div>
             </div>
