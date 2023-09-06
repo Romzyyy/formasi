f5ef4f93417cf0545217f2f3d59c4cb452b7715c
romzyyy <fijay0117@gmail.com>
2023-09-06 20:22:14 +0700

    style: add name and title avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -5,71 +5,74 @@ export default function Avatar() {
     return (
         <div
             id="Team"
-            className=" flex flex-wrap justify-between justify-items-center px-6 sm:px-20 lg:px-36 2xl:px-80 "
+            className=" flex flex-wrap justify-center px-6 sm:px-20 lg:px-36 2xl:px-80 "
         >
-            <div className="my-2 h-28 w-28 md:h-32 md:w-32 xl:h-40 xl:w-40 m-2">
-                <Image
-                    className=" h-full w-full object-cover"
-                    alt="image"
-                    width={1000}
-                    height={1000}
-                    src="/asd1.jpg"
-                />
+            <div className='my-2'>
+                <div className="m-2 my-2 h-28 w-28 md:mx-6 md:h-32 md:w-32 xl:h-40 xl:w-40">
+                    <Image
+                        className=" h-full w-full rounded-full object-cover"
+                        alt="image"
+                        width={1000}
+                        height={1000}
+                        src="/asd1.jpg"
+                    />
+                </div>
+                <p className="text-center font-semibold md:text-xl">jhon doe</p>
+                <p className="text-center">ketua</p>
             </div>
-            <div className="my-2  h-28 w-28 md:h-32 md:w-32 xl:h-40 xl:w-40 m-2">
-                <Image
-                    className=" h-full w-full object-cover"
-                    alt="image"
-                    width={1000}
-                    height={1000}
-                    src="/asd2.jpg"
-                />
+            <div className='my-2'>
+                <div className="m-2 my-2 h-28 w-28 md:mx-6 md:h-32 md:w-32 xl:h-40 xl:w-40">
+                    <Image
+                        className=" h-full w-full rounded-full object-cover"
+                        alt="image"
+                        width={1000}
+                        height={1000}
+                        src="/asd1.jpg"
+                    />
+                </div>
+                <p className="text-center font-semibold md:text-xl">jhon doe</p>
+                <p className="text-center">ketua</p>
             </div>
-            <div className="my-2  h-28 w-28 md:h-32 md:w-32 xl:h-40 xl:w-40 m-2">
-                <Image
-                    className="h-full w-full object-cover"
-                    alt="image"
-                    width={1000}
-                    height={1000}
-                    src="/asd3.jpg"
-                />
+            <div className='my-2'>
+                <div className="m-2 my-2 h-28 w-28 md:mx-6 md:h-32 md:w-32 xl:h-40 xl:w-40">
+                    <Image
+                        className=" h-full w-full rounded-full object-cover"
+                        alt="image"
+                        width={1000}
+                        height={1000}
+                        src="/asd1.jpg"
+                    />
+                </div>
+                <p className="text-center font-semibold md:text-xl">jhon doe</p>
+                <p className="text-center">ketua</p>
             </div>
-            <div className=" my-2 h-28 w-28 md:h-32 md:w-32 xl:h-40 xl:w-40 m-2 ">
-                <Image
-                    className=" sm:ml-0 h-full w-full object-cover"
-                    alt="image"
-                    width={1000}
-                    height={1000}
-                    src="/asd4.jpg"
-                />
+            <div className='my-2'>
+                <div className="m-2 my-2 h-28 w-28 md:mx-6 md:h-32 md:w-32 xl:h-40 xl:w-40">
+                    <Image
+                        className=" h-full w-full rounded-full object-cover"
+                        alt="image"
+                        width={1000}
+                        height={1000}
+                        src="/asd1.jpg"
+                    />
+                </div>
+                <p className="text-center font-semibold md:text-xl">jhon doe</p>
+                <p className="text-center">ketua</p>
             </div>
-            <div className="my-2  h-28 w-28 md:h-32 md:w-32 xl:h-40 xl:w-40 m-2">
-                <Image
-                    className=" sm:ml-0 h-full w-full object-cover"
-                    alt="image"
-                    width={1000}
-                    height={1000}
-                    src="/asd4.jpg"
-                />
+            <div className='my-2'>
+                <div className="m-2 my-2 h-28 w-28 md:mx-6 md:h-32 md:w-32 xl:h-40 xl:w-40">
+                    <Image
+                        className=" h-full w-full rounded-full object-cover"
+                        alt="image"
+                        width={1000}
+                        height={1000}
+                        src="/asd1.jpg"
+                    />
+                </div>
+                <p className="text-center font-semibold md:text-xl">jhon doe</p>
+                <p className="text-center">ketua</p>
             </div>
-            <div className="my-2  h-28 w-28 md:h-32 md:w-32 xl:h-40 xl:w-40 m-2">
-                <Image
-                    className=" sm:ml-0 h-full w-full object-cover"
-                    alt="image"
-                    width={1000}
-                    height={1000}
-                    src="/asd4.jpg"
-                />
-            </div>
-            <div className="my-2  h-28 w-28 md:h-32 md:w-32 xl:h-40 xl:w-40 m-2">
-                <Image
-                    className=" sm:ml-0 h-full w-full object-cover"
-                    alt="image"
-                    width={1000}
-                    height={1000}
-                    src="/asd4.jpg"
-                />
-            </div>
+
         </div>
     )
 }
