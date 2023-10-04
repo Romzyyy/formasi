e74d17e9f727c4a449ad3276b38505ade33d81f3
romzyyy <fijay0117@gmail.com>
2023-10-04 23:24:54 +0700

    style: add maps in contact

--- src/app/Fasilitas/Card.jsx
+++ src/app/Fasilitas/Card.jsx
@@ -1,24 +1,25 @@
-
-
 const information = [
   {
     title: "web developer",
-    content: "Web developer akan terlibat dalam proses pembuatan dari awal mulai dari mendesain tampilan hingga coding."
+    content:
+      "Web developer akan terlibat dalam proses pembuatan dari awal mulai dari mendesain tampilan hingga coding.",
   },
   {
     title: "Database Administrator",
-    content: "Pekerjaannya meliputi mengurutkan dan mengatur data dalam susunan tertentu untuk memudahkan pengguna data tertentu untuk mengaksesnya secara eksklusif."
+    content:
+      "Pekerjaannya meliputi mengurutkan dan mengatur data dalam susunan tertentu untuk memudahkan pengguna data tertentu untuk mengaksesnya secara eksklusif.",
   },
   {
     title: "Programmer",
-    content: "Dikutip dari Techopedia, programmer adalah individu yang membuat software atau aplikasi komputer dengan memberikan instruksi pemrograman khusus di komputer."
+    content:
+      "Dikutip dari Techopedia, programmer adalah individu yang membuat software atau aplikasi komputer dengan memberikan instruksi pemrograman khusus di komputer.",
   },
   {
     title: "System Engineer",
-    content: "Profesi ini bertugas untuk merancang dan mengimplementasikan software untuk berbagai informasi dan aplikasi berbasis teknologi serta prosedur pengujian untuk"
-  }
-]
-
+    content:
+      "Profesi ini bertugas untuk merancang dan mengimplementasikan software untuk berbagai informasi dan aplikasi berbasis teknologi serta prosedur pengujian untuk",
+  },
+];
 
 export default async function Card() {
   return (
@@ -30,8 +31,10 @@ export default async function Card() {
       <div className="grid md:grid-cols-2 xl:grid-cols-4 gap-4">
         {information.map((info) => (
           <div className="card w-full bg-gray-300 shadow-md bg-opacity-25 text-slate-800">
-            <div key={info.title} className="card-body rounded-md">
-              <h2 className="card-title text-base">{info.title}</h2>
+            <div className="card-body rounded-md">
+              <h2 key={info.title} className="card-title text-base">
+                {info.title}
+              </h2>
               <div className="bg-blue- h-24 w-full overflow-hidden">
                 <p>{info.content}</p>
               </div>
